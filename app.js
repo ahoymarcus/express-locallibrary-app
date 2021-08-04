bba30af1b6ebf060f7fb16bb19dafb8ef6f0672a
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-08-03 22:51:01 -0300

    Corrigir link

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 /*Setting up the DATABASE*/
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://marcus:Bhilll@cluster0.xykdh.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://marcus:<x>@cluster0.xykdh.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
